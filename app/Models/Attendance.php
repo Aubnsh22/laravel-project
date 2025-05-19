1215005829d6c003056e2d507fb8c771b8d333d0
saadas09 <hmssaad156@gmail.com>
2025-05-19 11:41:58 +0100

    ba3d

--- app/Models/Attendance.php
+++ app/Models/Attendance.php
@@ -3,17 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-
 
 class Attendance extends Model
 {
-    use HasFactory;
-
-    protected $fillable = ['user_id', 'clock_in', 'clock_out'];
-
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
+    //
 }
